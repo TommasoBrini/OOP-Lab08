a04ae8c33b0f1ff0f926fd15fffb4314d5fc114e
Tommaso <tommaso.brini@studio.unibo.it>
2021-01-19 16:51:58 +0100

    Solved ex3 mvcio

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -1,10 +1,20 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
 /**
  * 
  */
 public class Controller {
 
+    private static final String PATH = System.getProperty("user.home")
+            + System.getProperty("file.separator") + "Desktop"
+            + System.getProperty("file.separator")
+            + "output.txt";
+
+    private File currentFile = new File(PATH);
     /*
      * This class must implement a simple controller responsible of I/O access. It
      * considers a single file at a time, and it is able to serialize objects in it.
@@ -12,15 +22,44 @@ public class Controller {
      * Implement this class with:
      * 
      * 1) A method for setting a File as current file
-     * 
+     */
+
+    public final void setCurrentFile(final File file) {
+        if (file.getParentFile().exists()) {
+            this.currentFile = file;
+        } else {
+            throw new IllegalArgumentException("Not existing Path");
+        }
+    }
+
+    /* 
      * 2) A method for getting the current File
-     * 
+     */
+
+    public final File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    /* 
      * 3) A method for getting the path (in form of String) of the current File
-     * 
+     */
+
+    public final String getPath() {
+        return this.currentFile.getPath();
+    }
+
+    /* 
      * 4) A method that gets a String as input and saves its content on the current
      * file. This method may throw an IOException.
-     * 
-     * 5) By default, the current file is "output.txt" inside the user home folder.
+     */ 
+
+    public final void save(final String text) throws IOException {
+        try (PrintStream out = new PrintStream(currentFile)) {
+            out.println(text);
+        }
+    }
+
+     /* 5) By default, the current file is "output.txt" inside the user home folder.
      * A String representing the local user home folder can be accessed using
      * System.getProperty("user.home"). The separator symbol (/ on *nix, \ on
      * Windows) can be obtained as String through the method
